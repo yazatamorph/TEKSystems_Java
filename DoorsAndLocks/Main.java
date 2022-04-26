133ebc9b344c1d08dec34653fcdf7480eff9bee8
Zareen Price <yazatamorph@gmail.com>
2022-04-26 15:57:21 -0400

    Changed collection type

--- DoorsAndLocks/Main.java
+++ DoorsAndLocks/Main.java
@@ -2,7 +2,8 @@ package DoorsAndLocks;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Warehouse w = new Warehouse();
+        System.out.println(w);
     }
 
 } 
--- DoorsAndLocks/Warehouse.java
+++ DoorsAndLocks/Warehouse.java
@@ -3,14 +3,16 @@ package DoorsAndLocks;
 import java.util.*;
 
 public class Warehouse {
-    public Set<Door> doors = new TreeSet<Door>();
+    public ArrayList<Door> doors = new ArrayList();
 
     public Warehouse() {
         int i = 0;
+        // i is 0 through 9
         for (; i < 10; i++) {
             Door d = new Door(null);
             doors.add(d);
         }
+        // i is 10 through 99
         for (; i < 100; i++) {
             int num = (int)Math.random() * 4;
             Keyshape shape;
